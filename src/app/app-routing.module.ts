dabb2dfb4351def51c6da6d673296d1fa0c905c2
김도형 <dh0703.kim@samsung.com>
2022-07-14 00:26:36 +0900

    Update the packages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { OnnxComponent } from './onnx.component';
-import { RendererComponent } from './renderer.component';
+
+import { MainComponent } from './page/main.component';
+import { OnnxComponent } from './page/onnx.component';
+import { RendererComponent } from './page/renderer.component';
 
 const routes: Routes = [
-/*
-    { path: 'map',  component: MapComponent },
-    { path: 'board',  component: BoardComponent },
-*/
-    { path: "onnx", component: OnnxComponent },
-    { path: "renderer", component: RendererComponent },
-    { path: '**', redirectTo: 'onnx', pathMatch: 'full' }
+  { path: "main", component: MainComponent },
+  { path: "onnx", component: OnnxComponent },
+  { path: "renderer", component: RendererComponent },
+  { path: '**', redirectTo: 'main', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { APP_BASE_HREF } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { OnnxComponent } from './onnx.component';
-import { RendererComponent } from './renderer.component';
+
+import { MenuTopComponent } from './component/menu_top.component';
+import { MenuSideComponent } from './component/menu_side.component';
+import { MenuBottomComponent } from './component/menu_bottom.component';
+import { BannerSideComponent } from './component/banner_side.component';
+
+import { MainComponent } from './page/main.component';
+import { OnnxComponent } from './page/onnx.component';
+import { RendererComponent } from './page/renderer.component';
 
 import { Renderer } from './service/renderer';
 
 @NgModule({
   declarations: [
     AppComponent,
+
+    MenuTopComponent,
+    MenuSideComponent,
+    MenuBottomComponent,
+    BannerSideComponent,
+
+    MainComponent,
     OnnxComponent,
     RendererComponent
   ],
